11be3f6890c6268c0355b72911f3d1ec7e43d63b
Jeremy Yen <jeremy5189@gmail.com>
2018-12-02 15:14:31 +0800

    Setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as PIXI from 'pixi'
 
 // Create the renderer
-const renderer = PIXI.autoDetectRenderer(800, 600);
+const renderer = PIXI.autoDetectRenderer(1920, 1080);
 
 // Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
@@ -9,22 +9,21 @@ document.body.appendChild(renderer.view);
 // Create a container object called the `stage`
 const stage = new PIXI.Container();
 
-
 let style = {
-    font : 'bold italic 36px Arial',
-    fill : '#F7EDCA',
-    stroke : '#4a1850',
-    strokeThickness : 5,
-    dropShadow : true,
-    dropShadowColor : '#000000',
-    dropShadowAngle : Math.PI / 6,
-    dropShadowDistance : 6,
-    wordWrap : true,
-    wordWrapWidth : 440
+  font : 'bold italic 36px Arial',
+  fill : '#F7EDCA',
+  stroke : '#4a1850',
+  strokeThickness : 5,
+  dropShadow : true,
+  dropShadowColor : '#000000',
+  dropShadowAngle : Math.PI / 6,
+  dropShadowDistance : 6,
+  wordWrap : true,
+  wordWrapWidth : 440
 };
 
 const basicText = new PIXI.Text('It is working', style);
-basicText.x = 30;
+basicText.x = 1000;
 basicText.y = 90;
 
 stage.addChild(basicText);
