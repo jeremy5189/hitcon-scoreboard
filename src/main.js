4ba7451e903de4cc5ebbf69f72d33e2d7b53123a
Jeremy Yen <jeremy5189@gmail.com>
2018-12-02 16:18:22 +0800

    Fix

--- src/main.js
+++ src/main.js
@@ -19,17 +19,17 @@ var graphics = new PIXI.Graphics();
 
 // Draw red team
 graphics.lineStyle(0);
-graphics.beginFill(0xFF0000, 1);
-graphics.drawCircle(960, 1080, 100);
+graphics.beginFill(0xFF5330, 1);
+graphics.drawCircle(screen.w / 2, screen.h, 100);
 
 // Draw blue team
-graphics.beginFill(0x0000FF, 1);
+graphics.beginFill(0x5D94FF, 1);
 let radius = screen.h * 0.78; // 850
 let rad = Math.PI / 7;
 for (let t = 1; t <= 6; t++) {
   const x = radius * Math.cos(rad * t);
   const y = radius * -Math.sin(rad * t);
-  graphics.drawCircle(x + (screen.w / 2), y + (screen.h), 10);
+  graphics.drawCircle(x + (screen.w / 2), y + (screen.h), 50);
 }
 graphics.endFill();
 stage.addChild(graphics);
