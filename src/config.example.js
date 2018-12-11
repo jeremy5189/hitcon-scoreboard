dcf51e4293b99ae9b9f3b4e3d1d0561cd4fe607b
Jeremy Yen <jeremy5189@gmail.com>
2018-12-11 18:49:04 +0800

    Fix config example, ignore config.js

--- src/config.example.js
+++ src/config.example.js
@@ -1,7 +1,7 @@
 export default {
     api: 'http://localhost:8001',
     api_vtime: 'http://localhost:8002',
-    fetch_interval: 10000,
-    fetch_vtime_interval: 1000,
-    fast_render: false
+    fetch_interval: 10 * 1000,
+    fetch_vtime_interval: 30 * 1000,
+    fast_render: true
 };
--- src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    api: 'http://192.168.2.107:8001',
-    api_vtime: 'http://192.168.2.107:8002',
-    fetch_interval: 10000,
-    fetch_vtime_interval: 1000,
-    fast_render: true
-};
